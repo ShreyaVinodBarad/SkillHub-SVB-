cc20a8bb287aa915356048dbbbdfac042e153202
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-07-23 17:52:46 +0530

    23 July 2025 - Wednesday

--- SkillHub-Projects😉/09-June-2025(Batch)/Project-08(22-July-2025)/script.js
+++ SkillHub-Projects😉/09-June-2025(Batch)/Project-08(22-July-2025)/script.js
@@ -1,28 +1,36 @@
 const task = document.getElementById("taskInp");
 let taskList = document.getElementById("listOfTask");
-let toDoArr = [];
+// let toDoArr = [];
+let toDoArr = ["Learn HTML", "Learn CSS", "Learn JS"]; // Writing hard code...
 function setterForInp() {
     task.value = "";
 }
 function addTask() {
     let getTask = task.value;
-    toDoArr.push(getTask);
+    toDoArr.splice(0, 0, getTask);
     displayTasks();
     setterForInp();
 }
 function displayTasks() {
     let add = "";
+    let index = 0;
     for (let task of toDoArr) {
         add += `<div class = "alert alert-success d-flex justify-content-between align-items-center">
-        ${task} 
-        <button type = "button" class="btn btn-danger" id="delTask" onclick="deleteTask()">
+        ${task}
+        <button type = "button" class="btn btn-danger" id="delTask" onclick="deleteTask(${index})">
         Delete
         </button>
         </div>`;
+
+        index++;
     }
     taskList.innerHTML = add;
 }
 
-function deleteTask() {
-
+function deleteTask(i) {
+    toDoArr.splice(i, 1);
+    displayTasks();
+    // console.log(toDoArr);
 }
+
+displayTasks();
