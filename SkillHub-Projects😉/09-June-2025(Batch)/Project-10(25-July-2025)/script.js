92c3ebb8ca8fd2804691d0092540cd3fbbbc1c2a
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-08-02 17:54:01 +0530

    02 Aug 2025 - Saturday

--- SkillHub-Projects😉/09-June-2025(Batch)/Project-10(25-July-2025)/script.js
+++ SkillHub-Projects😉/09-June-2025(Batch)/Project-10(25-July-2025)/script.js
@@ -9,6 +9,14 @@ function setterForInp() {
     imgDes.value = "";
 }
 function add() {
+    /*
+    📌 If the key name and the variable name are the same, you can just write the name once inside the object.
+    👇
+    let imgTitle = document.getElementById("imgTitleInp").value;
+    let imgURL = document.getElementById("imgURLInp").value;
+    let imgDes = document.getElementById("imgDesInp").value;
+    imgDetailArr.splice(0, 0, { imgTitle, imgURL, imgDes });
+    */
     imgDetailArr.splice(0, 0, {
         titleImg: imgTitle.value,
         urlImg: imgURL.value,
@@ -16,6 +24,13 @@ function add() {
     });
     displayDetails();
     setterForInp();
+    document.getElementById("toast").innerHTML = `<div class ="alert alert-success">
+    Details of Image Added Successfully! 
+    </div>`
+    setTimeout(handleToast, 2000);
+    function handleToast() {
+        document.getElementById("toast").innerHTML = "";
+    }
 }
 function displayDetails() {
     let include = "";
@@ -29,7 +44,7 @@ function displayDetails() {
         <p>${details.desImg}</p>
         </div>
         </div>
-        <button type="button" class="btn btn-outline-danger" onclick = "deleteDetails(${index})">Delete</button>
+        <button type="button" class="btn btn-outline-danger" onclick = "deleteDetails(${index})">Remove</button>
         </div>`
         index++;
     }
@@ -38,4 +53,11 @@ function displayDetails() {
 function deleteDetails(indexNo) {
     imgDetailArr.splice(indexNo, 1);
     displayDetails();
+    document.getElementById("toast").innerHTML = `<div class ="alert alert-danger">
+    Details of Image Deleted Successfully! 
+    </div>`
+    setTimeout(handleToast, 2000);
+    function handleToast() {
+        document.getElementById("toast").innerHTML = "";
+    }
 }
