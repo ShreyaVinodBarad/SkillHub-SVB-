4876bd73ebd08c2e68d4d135c4e681a2450b6405
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-08-06 18:07:31 +0530

    06 Aug 2025 - Wednesday

---
+++ SkillHub-Projects😉/09-June-2025(Batch)/Project-13(06-Aug-2025)/script.js
@@ -0,0 +1,10 @@
+const API_Key = d4c4d289ec44bf44d8ac7c6021704aeb;
+let cityName = document.getElementById("city");
+let handleWeather = () => {
+    // console.log(cityName.value);
+    inpSetter();
+};
+
+let inpSetter = () => {
+    cityName.value = "";
+}
