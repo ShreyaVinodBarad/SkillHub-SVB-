f9dd99031769a75361a3223807a8ab7b8876b704
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-09-10 16:48:30 +0530

    10 Sept 2025 - Wednesday

---
+++ 09-June-2025(Batch)/September-2025/09-Sept-2025(Tuesday)/43_Python_Flask_ToDoApp/DeleteTaskLogic.py
@@ -0,0 +1,11 @@
+data = [
+    {"id": 1, "task": "Learn Python"},
+    {"id": 2, "task": "Learn JavaScipt"},
+    {"id": 3, "task": "Learn "},
+]
+tid = 2
+todos = []
+for item in data:
+    if item["id"] != tid:
+        print(todos.append(item))
+print(todos)
