42b808f800ee2340ec6b507782ab6e76d3edb73b
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2025-11-27 10:07:35 +0530

    27 Nov 2025 - Thursday

--- 09-June-2025(Batch)/November-2025/28-Nov-2025(Friday)/21_ReactJS_Part21/src/components/AdminNavBar.jsx
+++ 09-June-2025(Batch)/November-2025/28-Nov-2025(Friday)/21_ReactJS_Part21/src/components/AdminNavBar.jsx
@@ -1,30 +1,33 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { AuthContext } from '../App'
 
-const AuthorNavBar = () => {
-    const { auth } = useContext(AuthContext)
+const AdminNavBar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
-                        Author Panel
+                        Admin Panel
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <Link to="/author" class="nav-link active" href="#">
-                                Author Dashboard
+                            <Link to="/admin" class="nav-link active" href="#">
+                                Admin Dashboard
+                            </Link>
+                            <Link to="/admin/author" class="nav-link" href="#">
+                                Author
+                            </Link>
+                            <Link to="/admin/blogs" class="nav-link" href="#">
+                                Blogs
                             </Link>
                         </div>
                     </div>
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" >
-                            Welcome {auth.author.name}
-                            {/* 👆 auth is an object has two keys author and admin. So first go to author and then name. */}
+                            Welcome Admin
                         </button>
                         <ul class="dropdown-menu">
                             {/* <li><a class="dropdown-item" href="#">Action</a></li>
@@ -42,4 +45,4 @@ const AuthorNavBar = () => {
     )
 }
 
-export default AuthorNavBar
+export default AdminNavBar
---
+++ 09-June-2025(Batch)/November-2025/28-Nov-2025(Friday)/21_ReactJS_Part21/src/pages/AdminAuthor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminAuthor = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AdminAuthor
